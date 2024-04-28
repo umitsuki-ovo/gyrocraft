d42aa027fac2301459ad015804535f4bc004daf2
mia <85103168+mia-738@users.noreply.github.com>
2024-04-28 15:51:03 +0900

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,30 +1,11 @@
 import subprocess
 import datetime
-import psutil
-import shutil
-import os
 
-def server_kill(process_name):
-    process_pid = None
-    for proc in psutil.process_iter(['pid', 'name']):
-        if proc.info['name'] == process_name:
-            process_pid = proc.info['pid']
-            break
-        print('Process not found.')
-        break
-    psutil.Process(process_pid).kill()
 
 def start_minecraft():
     r = subprocess.Popen('LD_RIBRARY_PATH=. ./bedrock_server', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     for line in iter(r.stdout.readline,b''):
         terminal_log(line.rstrip().decode("utf8"))
-
-def backup():
-    server_kill('bedrock_server')
-    now_time = datetime.datetime.now()
-    shutil.copytree('./', f'../temp/{now_time.month}/{now_time}', dirs_exist_ok = True)
-    if os.path.isdir(f'../temp/{now_time.month + 7}') == True:
-        shutil.rmtree(f'../temp/{now_time.month + 7}')
 
 def terminal_log(terminal):
     logfile = '../temp/terminallog'
@@ -34,6 +15,4 @@ def terminal_log(terminal):
     with open(logfile, "a") as file:
         file.write(text)
 
-
-backup()
 start_minecraft()
