c383d4a81002bf044eec927105dfdc5848bc0401
mia <85103168+mia-738@users.noreply.github.com>
2024-04-28 11:36:03 +0900

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,9 +1,7 @@
 import subprocess
 import datetime
-import schedule
 import psutil
 import shutil
-import time
 import os
 
 def server_kill(process_name):
@@ -17,34 +15,25 @@ def server_kill(process_name):
     psutil.Process(process_pid).kill()
 
 def start_minecraft():
-    r = subprocess.popen('LD_RIBRARY_PATH=. ./bedrock_server', stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    r = subprocess.Popen('LD_RIBRARY_PATH=. ./bedrock_server', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     for line in iter(r.stdout.readline,b''):
         terminal_log(line.rstrip().decode("utf8"))
 
 def backup():
     server_kill('bedrock_server')
     now_time = datetime.datetime.now()
-    shutil.copytree('bedrock/', f'temp/{now_time.month}/{now_time}', dirs_exist_ok = True)
-    if os.path.isdir(f'temp/{now_time.month + 7}') == True:
-        shutil.rmtree(f'temp/{now_time.month + 7}')
+    shutil.copytree('./', f'../temp/{now_time.month}/{now_time}', dirs_exist_ok = True)
+    if os.path.isdir(f'../temp/{now_time.month + 7}') == True:
+        shutil.rmtree(f'../temp/{now_time.month + 7}')
 
 def terminal_log(terminal):
-    logfile = 'temp/'
+    logfile = '../temp/terminallog'
     now = datetime.datetime.now()
     text = f"{now}:   {terminal}\n"
 
-    with open(logfile, "terminallog") as file:
+    with open(logfile, "a") as file:
         file.write(text)
 
-def main():
-    backup()
-    start_minecraft()
-   
-start_minecraft()
-if __name__ == "__main__":
-    main()
 
-schedule.every.wednesday.at('12:00').do(main)
-while True:
-    schedule.run_pending()
-    time.sleep(1) 
+backup()
+start_minecraft()
