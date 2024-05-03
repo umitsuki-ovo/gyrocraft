a6f10586c0244fb536bea184c5861aae2190d304
mia <85103168+mia-738@users.noreply.github.com>
2024-05-04 08:20:50 +0900

    Update run.py

--- main/run.py
+++ main/run.py
@@ -1,12 +1,15 @@
 import subprocess
 import schedule
+import datetime
 import time
 
 def main():
-    subprocess.Popen("python3 main.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    subprocess.run("python3 backup.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    time.sleep(1)
+    subprocess.Popen('python3 main.py', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 if __name__ == '__main__':
-    subprocess.Popen("python3 main.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    subprocess.Popen('python3 main.py', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 schedule.every().wednesday.at('12:00').do(main)
 while True:
