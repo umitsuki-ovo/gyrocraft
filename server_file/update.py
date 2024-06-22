5ac3f751ba8490d698a4e21d7fefb460c345c5d1
mia <85103168+mia-738@users.noreply.github.com>
2024-06-22 12:35:02 +0900

    Update update.py

--- server_file/update.py
+++ server_file/update.py
@@ -15,7 +15,7 @@ filepath = ""
 
 def get_content():
     try:
-        response = requests.get("https://minecraft.net/en-us/download/server/bedrock/", timeout=30)
+        response = requests.get("https://minecraft.net/en-us/download/server/bedrock/", headers={"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15"}, verify=False, timeout=60)
         content = response.text
         return content
     except Timeout:
