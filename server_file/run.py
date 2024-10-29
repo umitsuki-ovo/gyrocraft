0dea62528212167eb13d6de5963374ef9460d6d1
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-10-29 13:18:50 +0900

    Update run.py

--- server_file/run.py
+++ server_file/run.py
@@ -4,19 +4,19 @@ import datetime
 import time
 
 def main():
-    subprocess.run("python3 update.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    subprocess.run("python3 ./update.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     time.sleep(1)
     if today.weekday() == 2:
         backup()
 
 def backup():
-    subprocess.run("python3 backup.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
+    subprocess.run("python3 ./backup.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     time.sleep(1)
-    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bedrockserver/'))
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bedrockserver'))
     subprocess.Popen('python3 main.py', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 if __name__ == '__main__':
-    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bedrockserver/'))
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bedrockserver'))
     subprocess.Popen('python3 main.py', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 schedule.every().day.at("12:00").do(main)
