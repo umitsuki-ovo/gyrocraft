b03761e777d208b38bad12e933068aec1c933429
umitsuki <85103168+umitsuki-ovo@users.noreply.github.com>
2024-10-29 13:12:38 +0900

    Update run.py

--- server_file/run.py
+++ server_file/run.py
@@ -4,17 +4,21 @@ import datetime
 import time
 
 def main():
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
     subprocess.run("python3 update.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     time.sleep(1)
     if today.weekday() == 2:
         backup()
 
 def backup():
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
     subprocess.run("python3 backup.py", shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
     time.sleep(1)
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bedrockserver/'))
     subprocess.Popen('python3 main.py', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 if __name__ == '__main__':
+    os.chdir(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'bedrockserver/'))
     subprocess.Popen('python3 main.py', shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT)
 
 schedule.every().day.at("12:00").do(main)
